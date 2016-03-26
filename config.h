112d03f02d63cfd00cf9041467de76c0f86b82c8
Tudor <xenogenesis@openmailbox.org>
2016-03-26 14:23:16 +0200

    Revert changes

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 
-#include "colors/woody.h"
+#include "colors/cloudy.h"
 
 static char font[] = "Metis,Symbola";
 static int bold_font = 0;
