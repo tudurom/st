f2f18b1ef054ec3758d361735b5f8485dd98ca76
Tudor <xenogenesis@openmailbox.org>
2016-04-17 01:56:50 +0300

    designr by dkeg

---
+++ colors/designr.h
@@ -0,0 +1,43 @@
+static const char *colorname[] = {
+  /* 8 normal colors */
+  "#2b3135",
+  "#a66959",
+  "#769070",
+  "#ac8d6e",
+  "#607a86",
+  "#80757e",
+  "#60867f",
+  "#c4c4b5",
+  "#4c5356",
+
+  /* 8 brightcolors */
+  "#a66959",
+  "#769070",
+  "#ac8d6e",
+  "#607a86",
+  "#80757e",
+  "#60867f",
+  "#c4c4b5",
+  "$COLOR_16$",
+
+  [255] = 0,
+
+  /* background and foreground colors */
+  [256] = "#20292d",
+  [257] = "#c4c4b5"
+};
+
+/* Default colors for foreground, background and cursor */
+static unsigned int defaultfg = 7;
+static unsigned int defaultbg = 0;
+static unsigned int defaultcs = 7;
+
+/*
+ * Colors used, when the specific fg == defaultfg. So in reverse mode this
+ * will reverse too. Another logic would only make the simple feature too
+ * complex.
+ */
+static unsigned int defaultitalic = 11;
+static unsigned int defaultunderline = 7;
+
+// vim: set ft=c :
