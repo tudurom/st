f92b31e4d5ea712d57492dc0e06cf7390f99abc1
Tudor <xenogenesis@openmailbox.org>
2016-05-01 21:51:27 +0300

    cloudy: change color 0 (!!)

--- colors/cloudy.h
+++ colors/cloudy.h
@@ -1,45 +1,35 @@
-/*
-
-┏━╸╻  ┏━┓╻ ╻╺┳┓╻ ╻
-┃  ┃  ┃ ┃┃ ┃ ┃┃┗┳┛
-┗━╸┗━╸┗━┛┗━┛╺┻┛ ╹
-by tudurom.
-
-*/
-
-/* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
   /* 8 normal colors */
-  "#273941",
+  "#456472",
   "#a66363",
   "#63a690",
   "#a6a663",
   "#6385a6",
-  "#bf9c86",
+  "#BF9C86",
   "#63a69b",
   "#c0c5ce",
 
-  /* 8 bright colors */
+  /* 8 brightcolors */
   "#456472",
   "#c27171",
   "#6dc2a3",
   "#bfc271",
   "#719bc2",
-  "#bf9c86",
+  "#BF9C86",
   "#71c2af",
   "#eff1f5",
 
   [255] = 0,
+
+  /* background and foreground colors */
+  [256] = "#273941",
+  [257] = "#c0c5ce"
 };
 
-
-/*
- * Default colors (colorname index)
- * foreground, background, cursor
- */
-static unsigned int defaultfg = 7;
-static unsigned int defaultbg = 0;
-static unsigned int defaultcs = 7;
+/* Default colors for foreground, background and cursor */
+static unsigned int defaultfg = 257;
+static unsigned int defaultbg = 256;
+static unsigned int defaultcs = 257;
 
 /*
  * Colors used, when the specific fg == defaultfg. So in reverse mode this
@@ -48,3 +38,5 @@ static unsigned int defaultcs = 7;
  */
 static unsigned int defaultitalic = 11;
 static unsigned int defaultunderline = 7;
+
+// vim: set ft=c :
